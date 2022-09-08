801f3575fb4b17d7353baa576de475a085e53329
Lazar <profkosmosic@gmail.com>
2022-09-08 18:18:38 +0200

    health script and UI

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]int health = 100;
+    [SerializeField]TextMeshProUGUI healthUI;
+
+    void Update() {
+        if(healthUI != null) {
+            healthUI.text = health + " HP".ToString();
+        }
+        if(health <= 0) {
+            Debug.Log("You're dead!");
+        }
+    }
+}
