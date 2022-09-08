02e20c690cc124e5186c5aaae486b43931469dd5
Lazar <profkosmosic@gmail.com>
2022-09-08 22:30:32 +0200

    split bullet script into two to fix bug

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField]float bulletSpeed;
+    [SerializeField]float bulletTime;
+    [SerializeField]int damage;
+    Health player;
+
+    void Start() {
+        player = FindObjectOfType<Health>();
+    }
+
+    void Update() {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+        Destroy(gameObject, bulletTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            Destroy(gameObject);
+            player.TakeDamage(damage);
+        }
+    }
+}
--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class PlayerBullet : MonoBehaviour
 {
     [SerializeField]float bulletSpeed;
     [SerializeField]float bulletTime;
     [SerializeField]int damage;
-    Health player;
-
-    void Start() {
-        player = FindObjectOfType<Health>();
-    }
 
     void Update() {
         transform.position += transform.forward * bulletSpeed * Time.deltaTime;
@@ -19,10 +14,6 @@ public class Bullet : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.tag == "Player") {
-            Destroy(gameObject);
-            player.TakeDamage(damage);
-        }
         if(other.tag == "Enemy") {
             Destroy(gameObject);
             Enemy enemy = other.transform.GetComponent<Enemy>();
