7ee625fa64d71b88be9adc1864edd82f390170ee
Lazar <profkosmosic@gmail.com>
2022-09-09 16:04:20 +0200

    added fire rate and ammo system

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,23 +6,39 @@ public class Gun : MonoBehaviour
 {
     [SerializeField]GameObject bullet;
     [SerializeField]ParticleSystem muzzleFlash;
+    [SerializeField]float fireRate = 12f;
+    [SerializeField]int maxAmmo = 12;
+    [SerializeField]int currentAmmo;
+    float nextTimeToFire = 0f;
     AudioSource gunshot;
 
     void Start()
     {
+        currentAmmo = maxAmmo;
         gunshot = gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) {
+        if(Input.GetKeyDown(KeyCode.R)) {
+            StartCoroutine(Reload());
+        }
+
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire && currentAmmo > 0) {
+            nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
         }
     }
 
     void Shoot() {
+        currentAmmo--;
         muzzleFlash.Play();
         gunshot.Play();
         GameObject shot = Instantiate(bullet, transform.position, transform.rotation);
+    }
+
+    IEnumerator Reload() {
+        yield return new WaitForSeconds(1f);
+        currentAmmo = maxAmmo;
     }
 }
