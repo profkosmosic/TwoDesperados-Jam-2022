f5abbf2350d03c709e9563d7046b12013364dcec
Lazar <profkosmosic@gmail.com>
2022-09-09 23:13:50 +0200

    refactoring bullet time script

--- Assets/Scripts/BulletTime.cs
+++ Assets/Scripts/BulletTime.cs
@@ -17,27 +17,34 @@ public class BulletTime : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.LeftShift) && !isSlow) {
-            slowmoIn.Play();
-            slowmoOut.Stop();
-            Time.timeScale = 0.3f;
-            mixer.SetFloat("pitch", 0.5f);
-            isSlow = true;
-            volume.profile.TryGetSettings(out g);
-            volume.profile.TryGetSettings(out b);
-            g.intensity.value = 1f;
-            b.intensity.value = 10f;
-
+            SlowMotionOn();
         }
         else if(Input.GetKeyDown(KeyCode.LeftShift) && isSlow) {
-            slowmoOut.Play();
-            slowmoIn.Stop();
-            Time.timeScale = 1f;
-            mixer.SetFloat("pitch", 1f);
-            isSlow = false;
-            volume.profile.TryGetSettings(out g);
-            volume.profile.TryGetSettings(out b);
-            g.intensity.value = 0.25f;
-            b.intensity.value = 3f;
+            SlowMotionOff();
         }
+    }
+
+    void SlowMotionOn() {
+        slowmoIn.Play();
+        slowmoOut.Stop();
+        Time.timeScale = 0.3f;
+        mixer.SetFloat("pitch", 0.5f);
+        isSlow = true;
+        volume.profile.TryGetSettings(out g);
+        volume.profile.TryGetSettings(out b);
+        g.intensity.value = 1f;
+        b.intensity.value = 10f;
+    }
+
+    void SlowMotionOff() {
+        slowmoOut.Play();
+        slowmoIn.Stop();
+        Time.timeScale = 1f;
+        mixer.SetFloat("pitch", 1f);
+        isSlow = false;
+        volume.profile.TryGetSettings(out g);
+        volume.profile.TryGetSettings(out b);
+        g.intensity.value = 0.25f;
+        b.intensity.value = 3f;
     }
 }
