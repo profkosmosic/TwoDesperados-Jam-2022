5ff967bb09f449c049ea42551a6a985b6a8e925a
Lazar <profkosmosic@gmail.com>
2022-09-11 00:35:00 +0200

    enemy and bullet sounds

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,14 @@ public class Enemy : MonoBehaviour
     [SerializeField]int health = 50;
     [SerializeField]ParticleSystem blood;
     [SerializeField]Animator anim;
+    AudioSource death;
     bool isDead = false;
     int deathAnim;
 
     void Start() {
         System.Random random = new System.Random ();
 		deathAnim = random.Next (1, 4);
+        death = gameObject.GetComponent<AudioSource>();
     }
 
     void Update() {
@@ -32,6 +34,7 @@ public class Enemy : MonoBehaviour
         else anim.SetTrigger("Death3");
         (gameObject.GetComponent("EnemyAI") as MonoBehaviour).enabled = false;
         this.GetComponent<BoxCollider>().enabled = false;
+        death.Play();
         isDead = true;
     }
 }
